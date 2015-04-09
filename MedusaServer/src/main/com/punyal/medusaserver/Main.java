d0a04e646765af29fbd04cdc8778b56ec1bf97d9
punyal <punyal@192.168.0.100>
2015-04-09 12:40:39 +0200

    First Structure

---
+++ MedusaServer/src/main/com/punyal/medusaserver/Main.java
@@ -0,0 +1,10 @@
+package com.punyal.medusaserver;
+
+import static com.punyal.medusaserver.core.Medusa.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Medusa Server " +
+                String.format("%d.%d", version, subVersion));
+    }
+}
---
+++ MedusaServer/src/main/com/punyal/medusaserver/core/Medusa.java
@@ -0,0 +1,7 @@
+package com.punyal.medusaserver.core;
+
+public class Medusa {
+    public static final int version = 0;
+    public static final int subVersion = 1;
+    
+}
